f8bd2fd45da40377882b580d6b5e67c32852b5f6
Togrul <togrulShahbazov@gmail.com>
2021-04-18 03:28:23 -0700

    admin panel yonetici paneli

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Backent Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+
+*/
+
+Route::get('/admin/panel',[App\Http\Controllers\Dashboard::class,'index'])->name('admin.dashboard');
+Route::get('/admin/giris',[App\Http\Controllers\Auth::class,'login'])->name('admin.login');
+
+/*
+|--------------------------------------------------------------------------
+| Front Routes
+|--------------------------------------------------------------------------
+
 */
 
 Route::get('/',[App\Http\Controllers\HomePageController::class,'index'])->name('homepage');
@@ -20,6 +26,8 @@ Route::post('/iletisim',[App\Http\Controllers\HomePageController::class,'contact
 Route::get('/kategory/{category}',[App\Http\Controllers\HomePageController::class,'category'])->name('category');
 Route::get('/{category}/{slug}',[App\Http\Controllers\HomePageController::class,'single'])->name('single');
 Route::get('/{sayfa}',[App\Http\Controllers\HomePageController::class,'page'])->name('page');
+
+
 
 
 
